ec01c6816e28c3947960828373eec66baf2e9231
Gift <Giftibe62@gmail.com>
2023-02-06 23:42:08 +0100

    01~

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import {v4 as uuidv4} from 'uuid'
-import {BrowserRouter as Router, Route,Routes} from 'react-router-dom';
+import {BrowserRouter as Router, NavLink, Route,Routes} from 'react-router-dom';
 import React from 'react';
+import Card from './components/shared/Card';
 import Header from './components/Header';
 import FeedbackList from './components/FeedbackList';
 import FeedbackData from './components/data/FeedbackData';
@@ -8,6 +9,7 @@ import { useState } from 'react';
 import FeedbackStats from './components/FeedbackStats';
 import FeedbackForm from './components/FeedbackForm';
 import AboutPage from './pages/AboutPage';
+import AboutIconLink from './components/AboutIconLink';
 
 function App(){
     const [feedback, setFeedback] = useState(FeedbackData)
@@ -30,20 +32,32 @@ function App(){
             <Route 
                 exact 
                 path='/'
-                    element ={
+                element ={
                         <>
                             <FeedbackForm handleAdd = {addFeedback} />
                             <FeedbackStats feedback={feedback}/>
                             <FeedbackList 
                             feedback ={feedback}
-                            handleDelete = {deleteFeedback}/>
+                            handleDelete = {deleteFeedback} />
+                            
                         </>
                     }>
                     
             </Route>    
 
-            <Route  path='/about' element={<AboutPage />}/> 
-            </Routes>   
+            <Route  
+            path='/about' 
+            element={<AboutPage />}
+            />
+            
+            </Routes>  
+            <Card>
+                <NavLink to={'/'} activeClassName={'active'}>
+                    Home
+                </NavLink>
+            </Card>
+            
+            <AboutIconLink />
             </div>
         </Router>
     )
